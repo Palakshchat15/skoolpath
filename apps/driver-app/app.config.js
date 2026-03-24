e7468e7fd8658354c88aa23cbd7b87ffc3c566c8
Palaksh Chaturvedi <palaksh1509@gmail.com>
2026-03-24 17:23:10 +0530

    env

---
+++ apps/driver-app/app.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  "expo": {
+    "name": "SkoolPath Driver",
+    "slug": "skoolpath-driver",
+    "scheme": "skoolpath-driver",
+    "entryPoint": "./index.js",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "platforms": [
+      "android",
+      "ios",
+      "web"
+    ],
+    "plugins": [
+      [
+        "expo-location",
+        {
+          "isAndroidBackgroundLocationEnabled": true
+        }
+      ],
+      "./withAndroidXCoreDowngrade.js"
+    ],
+    "ios": {
+      "supportsTablet": true,
+      "infoPlist": {
+        "NSLocationWhenInUseUsageDescription": "The driver app needs your location to share the bus position with parents.",
+        "NSLocationAlwaysAndWhenInUseUsageDescription": "The driver app needs background location so parents can track the bus during trips.",
+        "UIBackgroundModes": [
+          "location"
+        ]
+      }
+    },
+    "android": {
+      "package": "com.skoolpath.driver",
+      "config": {
+        "googleMaps": {
+          "apiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
+        }
+      }
+    },
+    "extra": {
+      "eas": {
+        "projectId": "b74df9cb-5289-4632-9814-1ffc682ad5e2"
+      }
+    }
+  }
+};
---
+++ apps/parent-app/app.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "expo": {
+    "name": "SkoolPath Parent",
+    "slug": "skoolpath-parent",
+    "scheme": "skoolpath-parent",
+    "entryPoint": "./index.js",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "platforms": [
+      "android",
+      "ios",
+      "web"
+    ],
+    "android": {
+      "package": "com.skoolpath.parent",
+      "config": {
+        "googleMaps": {
+          "apiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
+        }
+      }
+    },
+    "ios": {
+      "supportsTablet": true
+    },
+    "extra": {
+      "eas": {
+        "projectId": "8c6c8822-e274-45d8-af69-31d55fb695bf"
+      }
+    },
+    "plugins": [
+        "./withAndroidXCoreDowngrade.js"
+    ]
+  }
+};
